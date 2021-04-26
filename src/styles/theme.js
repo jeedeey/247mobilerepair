a2acdc83523ede861bf6a5c66fe51e6e66789652
jeedeey <speshies@gmail.com>
2021-04-26 13:08:14 +0100

    first commit

---
+++ src/styles/theme.js
@@ -0,0 +1,25 @@
+const { colors, fonts } = require("../../config")
+
+export const sharedPreferences = {
+  fonts: fonts,
+  breakpoints: {
+    xs: "480px",
+    sm: "768px",
+    md: "992px",
+    lg: "1200px",
+  },
+  borderRadius: "1rem",
+  pageWidth: "62.5rem",
+  headerHeight: "5rem",
+  footerHeight: "6rem",
+}
+
+export const lightTheme = {
+  ...sharedPreferences,
+  colors: colors.lightTheme,
+}
+
+export const darkTheme = {
+  ...sharedPreferences,
+  colors: colors.darkTheme,
+}
